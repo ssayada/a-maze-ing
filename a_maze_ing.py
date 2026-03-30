e7e04106b1dd113a0dce51c9722d0e9d1fd42690
ssayada <ssayada@student.42perpignan.fr>
2026-03-30 15:57:04 +0200

    modifs mais chaiplukoi

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from launcher import launcher
 
 
@@ -6,6 +7,14 @@ def main():
         launcher()
     except OSError as e:
         print(e)
+    except ValueError as e:
+        print(e)
+    except IndexError as e:
+        print(e)
+    except FileNotFoundError as e:
+        print(e)
+    except KeyError as e:
+        print("init_config.txt invalide ou illisible")
 
 
 if __name__ == "__main__":
